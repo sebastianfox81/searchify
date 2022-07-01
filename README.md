09e3b7c60ac9fee47471db0726ac244598365694
Sebastian <sebastianfox81@gmail.com>
2022-07-01 15:28:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Description
+
+- a search app that finds an existing github user and displays and array of information about the user including followers, repos, and charts to illustrate other traits about certain user.
+
+## Project Boot
+
+- install all packages via npm install
+- run 'npm start' in command line to open up application on local machine
+
+
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Dashboard, Login, Error } from './pages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
