50f54f0ecbef83fd93db04ad470728d396c1e016
Sebastian <sebastianfox81@gmail.com>
2022-07-01 16:03:49 -0400

    renamed title head

--- src/context/context.js
+++ src/context/context.js
@@ -73,7 +73,7 @@ const GithubProvider = ({ children }) => {
   useEffect(checkRequests, [])
   // get initial user
   useEffect(() => {
-    searchGithubUser('fabpot')
+    searchGithubUser('mikebordelon')
   }, [])
 
   return (
